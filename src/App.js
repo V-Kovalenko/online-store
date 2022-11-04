60b095fe77f9c39d54dfbf0cfa238622591adddc
V-Kovalenko <gorsetis@yandex.ru>
2022-11-04 20:24:22 +0700

    feat(init): project setting up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import classes from './App.module.css';
+import {Provider} from "react-redux";
+import {store} from "./redux/store";
+import {MainPage} from "./components/pages/MainPage/MainPage";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import {Nav} from "./components/Nav/Nav";
+import {Basket} from "./components/pages/Basket/Basket";
+import {Logs} from "./components/pages/Logs/Logs";
+import {Header} from "./components/Header/Header";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Provider store={store}>
+            <BrowserRouter>
+                <div className={classes.main}>
+                    <Header/>
+                    <Nav/>
+                    <div className={classes.content}>
+                        <Routes>
+                            <Route path='mainPAge' element={<MainPage/>}/>
+                            <Route path='basket' element={<Basket/>}/>
+                            <Route path='logs' element={<Logs/>}/>
+                        </Routes>
+                    </div>
+                </div>
+
+            </BrowserRouter>
+
+        </Provider>
+
+    );
 }
 
 export default App;
