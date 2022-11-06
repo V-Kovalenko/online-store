e29723c7c2db0aebdc982a55c258e60b577e3dac
V-Kovalenko <gorsetis@yandex.ru>
2022-11-06 22:20:23 +0700

    get cards from backend

--- src/components/pages/MainPage/MainPage.jsx
+++ src/components/pages/MainPage/MainPage.jsx
@@ -1,21 +1,26 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import classes from './MainPage.module.css'
-import {cards} from '../../../redux/cards/cards';
 import Card from '../../Cards/Card'
 
 
 export const MainPage = () => {
-    const [myCards, setMyCards] = useState(cards)
+    const [myCards, setMyCards] = useState([])
+    useEffect(() => {
+        fetch('https://http-test-two.vercel.app/api/v1/cards?page=1').then(it => it.json())
+            .then(it => setMyCards(it.result))
+            .catch(err => console.error(err))
+    }, [])
+
 
     return (
         <div className={classes.mainDiv}>
             <div className={classes.form}>
-                    <span>Сортировать по</span>
-                    <select className={classes.select}>
-                        <option disabled>Sort</option>
-                        <option>По цене</option>
-                        <option>По названию</option>
-                    </select>
+                <span>Сортировать по</span>
+                <select className={classes.select}>
+                    <option disabled>Sort</option>
+                    <option>По цене</option>
+                    <option>По названию</option>
+                </select>
 
 
             </div>
